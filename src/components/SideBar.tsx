986b9db50194c5d45bd8c2978468b99804757dd1
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-23 16:53:07 -0300

    componetize application challenge done. (im so clean goddammm)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ selectedGenreIdVariable, handleClickButtonFunction}: { selectedGenreIdVariable: number, handleClickButtonFunction: (id: number) => void}) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButtonFunction(genre.id)}
+            selected={selectedGenreIdVariable === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
